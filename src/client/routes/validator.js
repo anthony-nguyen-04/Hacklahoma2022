6a7d3fe21a85c824a9353c95a966f0d3a952e1e7
sam-irl <sambirdthenerd@gmail.com>
2022-02-13 03:50:35 -0600

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/client/routes/validator.js
+++ src/client/routes/validator.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('validator/scan');
 });
+
+router.post('/submit', function (req, res, next) {
+  res.send(req.body.data)
+})
 
 module.exports = router;
